1ebb38b69913e47350486975ef931a066b4e5ad7
Meagan <meaganpartington@gmail.com>
2026-02-25 15:43:38 -0500

    Completed the code for the sequentiel part, theres only the cocurrence left.

--- matching.go
+++ matching.go
@@ -9,8 +9,10 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // The Resident data type
@@ -20,6 +22,7 @@ type Resident struct {
 	lastname string
 	rol []string 			// resident rank order list
 	matchedProgram string	// will be "" for unmatched resident
+	nextOffer int  //index du prochain programme a contacter dans ROL
 }
 
 // The Program data type
@@ -28,7 +31,7 @@ type Program struct {
 	name       string
 	nPositions  int 		// number of positions available (quota)
 	rol []int  				// program rank order list
-	// TO ADD: a data structure for the selected resident IDs
+	selectedResidents []int //TO ADD : liste des IDs des residents selectionner
 }
 
 // Parse a resident's ROL
@@ -161,29 +164,143 @@ func ReadProgramsCSV(filename string) (map[string]*Program, error) {
 	return programs, nil
 }
 
+//getRank return la position d'un resident dans la ROL d'un programme
+func getRank(rol []int, rid int) int {
+	for i, id := range rol {
+		if id == rid { //si on trouve le resident dans la liste, return son rang
+			return i
+		}
+	}
+	return len(rol) //le resident n'est pas dans la liste (aka rang tres bas donc non preferer)
+}
+
+
+//algo McVittie-Wilson ver sequentielle
+func offer(rid int, residents map[int]*Resident, programs map[string]*Program) { //le resident rid fait un offre au prochain rpogrammde de sa ROL
+	resident := residents[rid] //obtenir le resident depuis le map
+
+	if resident.nextOffer >= len(resident.rol) { //si le resident a contacter tout ses programmes il reste
+		return
+	}
+
+	pid := resident.rol[resident.nextOffer] //obetnir l'ID du prochain programme a contacter
+	resident.nextOffer++ //avance l'index pour que le prochain attempt contacte le programme suivant
+	evaluate(rid, pid, residents, programs) //le programme evalue l'offre du resident
+}
+
+func evaluate(rid int, pid string, residents map[int]*Resident, programs map[string]*Program) { //le programme pid evalue l'offre du resident rid
+	program := programs[pid] //obtenir le programme du map
+
+	if len(program.selectedResidents) < program.nPositions { //soit le programme a encore des places libres
+		program.selectedResidents = append(program.selectedResidents, rid) //accepter le resident donc l'ajouter a la liste des selectionnes
+		residents[rid].matchedProgram = pid //marque le resident comme matched au programm
+
+	} else { //ou soit le programme est plein, ont trouve le resident le moins preferer
+		worstRank := -1 //rang du pire resident actuellement selectionne
+		worstRid := -1 //ID du pire resident selectionne
+
+		for _, currentRid := range program.selectedResidents {
+			rank := getRank(program.rol, currentRid) //calculet le rang de chaque resident selectionne dans le ROL du programme
+			if rank > worstRank { //store le resident avec le rang le plus mauvais
+				worstRank = rank
+				worstRid = currentRid
+			}
+		}
+
+		newRank := getRank(program.rol, rid) //calculer le rang du nouveau resdient dnas le ROL du programme
+		if newRank < worstRank { //si le programme preefere le nouveau resident 
+			for i, id := range program.selectedResidents { //remplacer le pire resident par le nouveau dans la liste des selectionnes
+				if id == worstRid {
+					program.selectedResidents[i] = rid
+					break
+				}
+			}
+			
+			residents[rid].matchedProgram = pid //mettre a jour le nouveau resident accepte
+			residents[worstRid].matchedProgram = "" //le resident rejeter devient libre
+			offer(worstRid, residents, programs) //le resident rejeter doit iare une nouvelle offre 
+
+		} else { 
+			offer(rid, residents, programs) //le programme prefere ses residents actuels 
+		}
+	}
+}
+
+func printResults(residents map[int]*Resident, programs map[string]*Program) {
+	//structure pour stocker une ligne de resultat
+	type result struct {
+		lastname    string
+		firstname   string
+		residentID  int
+		programID   string
+		programName string
+	}
+
+	var results []result //liste de toutes les lignes de resultats
+	unmatchedCount := 0  //compteur de residents non matched
+
+	//parcourir tous les residents pour construire les lignes
+	for _, r := range residents {
+		pid := r.matchedProgram
+		pname := "NOT_MATCHED" //valeur par defaut si le resident n'est pas matched
+		if pid == "" {
+			//resident non matched : utiliser "XXX" comme identifiant de programme
+			pid = "XXX"
+			unmatchedCount++
+		} else {
+			//obtenir le nom du programme depuis la map
+			pname = programs[pid].name
+		}
+		//ajouter le resultat a la liste
+		results = append(results, result{r.lastname, r.firstname, r.residentID, pid, pname})
+	}
+
+	//trier les resultats par nom de famille en ordre alphab
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].lastname < results[j].lastname
+	})
+
+	fmt.Println("lastname,firstname,residentID,programID,name")
+	//afficher chaque ligne de resultat
+	for _, res := range results {
+		fmt.Printf("%s,%s,%d,%s,%s\n", res.lastname, res.firstname, res.residentID, res.programID, res.programName)
+	}
+
+	//calculer le nombre total de places non remplies dans tous les programmes
+	availablePositions := 0
+	for _, p := range programs {
+		availablePositions += p.nPositions - len(p.selectedResidents)
+	}
+
+	//afficher les statistiques finales
+	fmt.Printf("Number of unmatched residents: %d\n", unmatchedCount)
+	fmt.Printf("Number of positions available: %d\n", availablePositions)
+}
+
 // Example usage
 func main() {
 
     // read residents
-	residents, err := ReadResidentsCSV("residents4000.csv")
+	residents, err := ReadResidentsCSV("residents.csv")
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	for _, p := range residents {
-		fmt.Printf("ID: %d, Name: %s %s, Rol: %v\n", p.residentID, p.firstname, p.lastname, p.rol)
-	}
-	
-	programs, err := ReadProgramsCSV("programs4000.csv")
+	programs, err := ReadProgramsCSV("programs.csv") //read program
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	for _, p := range programs {
-		fmt.Printf("ID: %s, Name: %s, Number of pos: %d, Number of applicants: %d\n", p.programID, p.name, p.nPositions, len(p.rol))
+	start := time.Now() //debut du chronometre
+
+	for id := range residents { //appeler offer pour cahque resident pour lancer l'algo
+		offer(id, residents, programs)
 	}
 
-    fmt.Printf("\nNMD: %v",programs["NMD"])
+	end := time.Now() //fin du chronometre
+
+	printResults(residents, programs) //afficher dans le format
+	fmt.Printf("\nExecution time: %s\n", end.Sub(start))
 }
