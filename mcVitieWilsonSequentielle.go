f1c27b8cc6a6f91ec52c5563c08261922cc724bb
Anastasia <annie.sadovski@gmail.com>
2026-02-25 15:43:59 -0500

    Added all necessary documents

---
+++ mcVitieWilsonSequentielle.go
@@ -0,0 +1,10 @@
+// Project CSI2120/CSI2520
+// Partie 2 (Go)
+// Winter 2026 - 1 mars, 2026
+// Robert Laganiere, uottawa.ca
+
+// Complété par :
+// Meagan Partington - 300416906
+// Anastasia Sadovskyy - 300426037
+
+
